2ed8fbd5e8389251b87856d65b6a428ee9226816
prx <1803874853@qq.com>
2019-10-19 17:58:41 +0800

    第一次提交

--- app/src/main/java/smallyue/com/testproject/MainAty.java
+++ app/src/main/java/smallyue/com/testproject/MainAty.java
@@ -2,9 +2,11 @@ package smallyue.com.testproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainAty extends AppCompatActivity {
 
+    private TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
